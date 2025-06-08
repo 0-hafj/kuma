334a653266f76235c45e0c2db6a4e6676e7525c1
Håvard Fjukstad <havard.fjukstad@dfo.no>
2025-06-08 13:34:35 +0200

    added 20 days retention to backup script

--- backup.sh
+++ backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+find /home/anonymous/kuma/backup/ -type f -name '*.tar.gz' -mtime +20 -exec rm -v {} \;
+
 NAMESPACE=kuma
 DEPLOYMENT=kuma-deploy
 BACKUP_POD=kuma-backup
 BACKUP_FILE=$(date +%Y\%m\%d_\%H\%M\%S)_kuma-data.tar.gz
+
+kubectx tlo-prod-aks
 
 kubectl scale deployment $DEPLOYMENT --replicas=0 -n $NAMESPACE
 
@@ -13,7 +17,7 @@ kubectl apply -f /home/anonymous/kuma/4_backup_pod.yaml
 
 kubectl wait --for=condition=Ready pod/$BACKUP_POD -n $NAMESPACE --timeout=120s
 
-sleep 30
+sleep 60
 
 kubectl -n $NAMESPACE cp $BACKUP_POD:/backup/kuma-data.tar.gz /home/anonymous/kuma/backup/$BACKUP_FILE 
 
